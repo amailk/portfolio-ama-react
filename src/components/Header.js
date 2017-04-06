af7651e017ec3d18e4ddd5a62a772ae4dde427a4
Ama Liyanage <liyaniama@gmail.com>
2017-04-06 19:38:33 -0400

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Header = (props) => (
+  <div>
+    <h1>Ama Liyanage</h1>
+    <h2>Front end developer</h2>
+    <div>
+      <p>Icons go here</p>
+    </div>
+  </div>
+);

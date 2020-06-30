4b83489596e96a776e283ed001456c5efb81fc49
Ama Liyanage <liyaniama@gmail.com>
2020-06-30 18:01:15 -0400

    Create React App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import {Header, ProjectList} from './components'
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-var data = require("../db.json");
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header/>
-        <ProjectList projects={data.projects}/>
-        <footer>Ama Liyanage 2016</footer>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export {Header} from './Header'
-export {ProjectList} from './ProjectList'
-export {Project} from './Project'
-export {Contacts} from './Contacts'
